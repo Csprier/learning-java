9daac6755ea7755cf055758d01d27d2b91276269
Cam Prier <CSPrier@gmail.com>
2020-09-13 19:52:32 -0400

    Started describing Reference Data Types

--- com/cspcode/Main.java
+++ com/cspcode/Main.java
@@ -1,30 +1,31 @@
 package com.cspcode;
 
+import java.time.LocalDate;
+
 public class Main { // The Main class is the main entry point for any Java program
 
-  public static void main(String[] args) { // main is a method in the class Main
+  public static void main(String[] args) { // main is a method in the Main class
     System.out.println("Hello, world!"); // System.out.println(); prints to the console
 
     /* Primitive Data Types */
     byte numberInBytes = -128;        // 1 byte.  Minimum value of -128, maximum value of 127(inclusive).
-    short shortNumber = 32_767;        // 2 bytes. Minimum value of -32768, max of 32767.
+    short shortNumber = 32_767;       // 2 bytes. Minimum value of -32768, max of 32767.
     int number = 100;                 // 4 bytes. Integer, Max value of 4,294,967,295.
-    long longNumber = 1_000_000L;       // 8 bytes. Max value of 18 quintillion+.
+    long longNumber = 1_000_000L;     // 8 bytes. Max value of 18 quintillion+.
     float floatingPointNumber = 0.0F; // 4 bytes. Floats are used for 7-digit decimal numbers, and require less memory.
     double decimalNumber = 100.01;    // 8 bytes. 15-digit Decimal numbers.
     char letter = 'A';                // 2 bytes. Single 16-bit unicode character, with single-quotes.
     boolean yes = true;               // 1 bit.   True/false values only.
     boolean no = false;
-    String seriesOfCharacters = "This is a string"; // A series of unicode characters, with double-quotes.
 
     /* Reference Data Types */
-
-
-
+    String name = new String("CspCode"); // Sequence of chars.
+    LocalDate now = LocalDate.now();             // LocalDate object, with additional methods.
   }
 }
 
-/*================================================================================= //
+/*
+==================================================================================================
  * Running(Compiling) a Java program from the terminal *
  --------------------------------------------
   From the 'src' directory
@@ -33,4 +34,5 @@ public class Main { // The Main class is the main entry point for any Java progr
   Second, from the same directory, run "$ java <fully qualified name>"
   e.g.
   "$ java com.cspcode.Main" <-- this invokes the 'Main.class' file, not the 'Main.java' file
-================================================================================= */
+==================================================================================================
+*/
